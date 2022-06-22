a1deef705e7f4b303097901818a6f58d887e4ba4
Ahmed Al-Hasanat <30767394+ahmedhasanat97@users.noreply.github.com>
2022-06-22 16:43:21 +0300

    Initial commit

---
+++ src/main/java/com/example/zicha/backend/ZichaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zicha.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZichaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZichaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zicha/backend/ZichaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zicha.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZichaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
